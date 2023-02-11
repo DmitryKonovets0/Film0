f3917fa5f85d7b42e837aacef3e0af2daa846b03
Dmitry Konovets <114726643+DmitryKonovec0@users.noreply.github.com>
2023-02-11 23:37:47 +0200

    Second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,68 @@
-const numberOfFilms = prompt('How many films you already watched?'); 
+let numberOfFilms;
+
+function start(){
+
+    numberOfFilms = +prompt('How many films you already watched?');
+
+    while(numberOfFilms == '' || numberOfFilms == null ||isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('How many films you already watched?');
+    }
+
+}
+start()
+
 const personalMovieDb = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     generes: [],
     privat: false
-};
+}
 
-const a = prompt('One of last watched films'),
-      b = prompt('How match do you rate it'),
-      c = prompt('One of last watched films'),
-      d = prompt('How match do you rate it');
+function showMyDB(){
+    if (personalMovieDb.privat == false ){
+            console.log(personalMovieDb);
+    }
+}
+    showMyDB();
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
 
+function rememberMyFilms() {
 
+    for (let i = 0; i < 2; i++){
+        const watched_films = prompt('One of last watched films'),
+              rate = prompt('How match do you rate it');
+
+    if(watched_films !== '' && rate !== '' && watched_films !== null && rate !== null && watched_films.length < 50){
+                console.log('cool');
+                personalMovieDb.movies [watched_films] = rate;
+            }else{
+                console.log('error');
+                i--
+             }
+        }
+}rememberMyFilms()
+
+function detectedPersonalLevel() {
+
+    if (personalMovieDb.count < 10) {
+        console.log('watched quite few film');
+    } else if(personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+        console.log("you are a classic spectator");
+    } else if(personalMovieDb.count >= 30 ) {
+        console.log('you are a cinephile');
+    }else{
+        console.log('Error');
+    }
+
+}
+detectedPersonalLevel()
+
+function writeYourGenres(){
+    for(let i = 1; i < 4; i++){
+        let answer = prompt(`Your favorite genre is numbred ${i}`);
+        personalMovieDb.generes[i-1] = answer
+    }
+
+}writeYourGenres()
+console.log(personalMovieDb);
